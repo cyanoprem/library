a4bd7527e14a8cbf9e2c39231fab9473cc4d29b8
Cyano Prem <66469412+cyanoprem@users.noreply.github.com>
2023-05-17 21:48:49 +0530

    Load presaved book list

---
+++ script.js
@@ -0,0 +1,34 @@
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+
+const book1 = new Book('Sales EQ', 'Jeb Blount', 288, 'Yes')
+const book2 = new Book('Hooked', 'Nir Eyal', 171, 'Yes')
+const book3 = new Book('Cloud Native Apps on GCP', 'Alasdair Gilchrist', 401, 'No')
+
+let myLibrary = [book1, book2, book3]
+
+const books = document.getElementById('books')
+
+for (let i = 0; i < myLibrary.length; i++) {
+  let book = document.createElement('li')
+  book.innerHTML = myLibrary[i].title
+  books.appendChild(book)
+}
+
+// try {
+//   function addBookToLibrary(books, inputBook) {
+//     console.log(inputBook.value);
+//     let newBook = document.createElement('li')
+//     newBook.innerHTML = inputBook.value
+//     books.appendChild(newBook)
+//     return false 
+//   }
+// } catch (error) {
+//   console.log(error)
+// }
+
