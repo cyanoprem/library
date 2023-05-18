a97904080bd7683cb33453bb3d3dc39ff9402704
Cyano Prem <66469412+cyanoprem@users.noreply.github.com>
2023-05-18 17:24:24 +0530

    Add modal

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const books = document.getElementById('books')
 for (let i = 0; i < myLibrary.length; i++) {
   let book = document.createElement('li')
   book.innerHTML = `Title: ${myLibrary[i].title}, Author: ${myLibrary[i].author}, No of Pages: ${myLibrary[i].pages}, Read: ${myLibrary[i].read}`
+  book.classList.add('card', 'column', 'm-2')
   books.appendChild(book)
   let deleteButton = document.createElement("button")
   deleteButton.appendChild(document.createTextNode("delete"))
@@ -41,6 +42,7 @@ document.querySelector('form.new-book-form').addEventListener('submit', function
     let newBookObject = new Book(newBook.value, author.value, parseInt(pages.value), read.value)
     myLibrary.push(newBookObject)
     book.innerHTML = `Title: ${newBook.value}, Author: ${author.value}, No of Pages: ${parseInt(pages.value)}, Read: ${read.value}`
+    book.classList.add('card', 'column', 'm-2')
     books.appendChild(book)
     let deleteButton = document.createElement("button")
     deleteButton.appendChild(document.createTextNode("delete"))
@@ -48,5 +50,55 @@ document.querySelector('form.new-book-form').addEventListener('submit', function
     deleteButton.onclick = function() {
       this.parentElement.style.display = "none";
     }
-    document.querySelector('.new-book-form').reset()    
+    document.querySelector('.new-book-form').reset()
+    const modal = document.getElementById('modal-js-example')
+    modal.classList.remove('is-active');
 })
+
+
+// JS for Modal
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button .submitBookBtn') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    // if (e.keyCode === 27) { // Escape key
+    //   closeAllModals();
+    // }
+  });
+});
