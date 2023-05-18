0a9ba06ae458cf0f93ba4faaac059e1f3bd5e884
Cyano Prem <66469412+cyanoprem@users.noreply.github.com>
2023-05-18 22:17:15 +0530

    Add sytles to cards

--- script.js
+++ script.js
@@ -22,13 +22,39 @@ function loadBooks () {
   for (let i = 0; i < myLibrary.length; i++) {
     let book = document.createElement('li')
 
-    book.innerHTML = `Title: ${myLibrary[i].title}, Author: ${myLibrary[i].author}, No of Pages: ${myLibrary[i].pages}, Read: ${myLibrary[i].read}`
+    let bookTitle = document.createElement('div')
+    bookTitle.innerHTML = myLibrary[i].title
+    bookTitle.classList.add('title', 'is-4')
+    book.appendChild(bookTitle)
 
-    book.classList.add('card', 'column', 'm-2')
-    books.appendChild(book)
+    let bookAuthor = document.createElement('div')
+    bookAuthor.innerHTML = `by ${myLibrary[i].author}`
+    bookAuthor.classList.add('subtitle', 'is-6', 'is-italic')
+    book.appendChild(bookAuthor)
+
+    let pageNo = document.createElement('div')
+    pageNo.innerHTML = `No of Pages: ${myLibrary[i].pages}`
+    pageNo.classList.add('content')
+    book.appendChild(pageNo)
+
+    let didRead = document.createElement('div')
+    didRead.innerHTML = `Read: ${myLibrary[i].read}`
+    didRead.classList.add('content')
+    book.appendChild(didRead)
+
+
+    let column = document.createElement('div')
+    column.classList.add('column')
+    
+
+    book.classList.add('card', 'card-content','is-half')
+    column.appendChild(book)
+
+    books.appendChild(column)
 
     let deleteButton = document.createElement("button")
-    deleteButton.appendChild(document.createTextNode("delete"))
+    deleteButton.appendChild(document.createTextNode("Delete"))
+    deleteButton.classList.add('button', 'is-danger')
     book.appendChild(deleteButton)
 
     deleteButton.onclick = function() {
@@ -66,13 +92,38 @@ document.querySelector('form.new-book-form').addEventListener('submit', function
     let newBookObject = new Book(newBook.value, author.value, parseInt(pages.value), read.value)
     addBookToLibrary(newBookObject)
 
-    book.innerHTML = `Title: ${newBook.value}, Author: ${author.value}, No of Pages: ${parseInt(pages.value)}, Read: ${read.value}`
+    let bookTitle = document.createElement('div')
+    bookTitle.innerHTML = newBook.value
+    bookTitle.classList.add('title', 'is-4')
+    book.appendChild(bookTitle)
 
-    book.classList.add('card', 'column', 'm-2')
-    books.appendChild(book)
+    let bookAuthor = document.createElement('div')
+    bookAuthor.innerHTML = `by ${author.value}`
+    bookAuthor.classList.add('subtitle', 'is-6', 'is-italic')
+    book.appendChild(bookAuthor)
+
+    let pageNo = document.createElement('div')
+    pageNo.innerHTML = `No of Pages: ${parseInt(pages.value)}`
+    pageNo.classList.add('content')
+    book.appendChild(pageNo)
+
+    let didRead = document.createElement('div')
+    didRead.innerHTML = `Read: ${read.value}`
+    didRead.classList.add('content')
+    book.appendChild(didRead)
+
+    let column = document.createElement('div')
+    column.classList.add('column')
+    
+
+    book.classList.add('card', 'card-content', 'is-half')
+    column.appendChild(book)
+
+    books.appendChild(column)
 
     let deleteButton = document.createElement("button")
-    deleteButton.appendChild(document.createTextNode("delete"))
+    deleteButton.appendChild(document.createTextNode("Delete"))
+    deleteButton.classList.add('button', 'is-danger')
     book.appendChild(deleteButton)
     deleteButton.onclick = function() {
       this.parentElement.style.display = "none";
